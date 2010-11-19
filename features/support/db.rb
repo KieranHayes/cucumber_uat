d1b3b87819d45ed571aaba1ec3dc1962ce091886
Christian Trabold <info@christian-trabold.de>
2010-11-19 18:48:12 +0100

    Added example for ActiveRecord-Adapter

---
+++ features/support/db.rb
@@ -0,0 +1,18 @@
+## establish a connection to the database
+#ActiveRecord::Base.establish_connection(
+#	:adapter  => "mysql",
+#	:host     => "127.0.0.1",
+#	:Database => "db_name",
+#	:username => "root",
+#	:Password => "",
+#	:Port => 3306,
+#	:encoding => "utf8",
+#	:collation => "utf8"
+#)
+#
+#class Press < ActiveRecord::Base
+##  establish_connection (red_dot_db)
+##  establish_connection (typo3_db)
+#	set_table_name 'tt_news'
+#	set_primary_key 'uid'
+#end
