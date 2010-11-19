688dd92b42f2feecc151d5b6e5fa016bd3cebcab
Christian Trabold <info@christian-trabold.de>
2010-11-19 18:50:18 +0100

    Improved examples

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,36 +3,47 @@ unless ENV['CUCUMBER_HOST']
 #  raise 'You must set CUCUMBER_HOST environment variable with the name of your host used to run cucumber features'
 end
 
-## ActiveRecord
-#require 'active_record'
-#
-## RSpec
-#require 'spec/expectations'
-#
-## Caybara
-#require 'capybara/cucumber'
-#require 'capybara/session'
-#
-##Selenium
-#require 'selenium-webdriver'
+# ActiveRecord
+require 'active_record'
 
-# Webrat
-#require "capybara/rack-test"
-#require "capybara/envjs"
+# RSpec
+require 'spec/expectations'
+
+# Caybara
+require 'capybara/cucumber'
+require 'capybara/session'
+
+#Selenium
+require 'selenium-webdriver'
+
+# Caybara
+#require 'httparty'
+require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
+#require 'cucumber/web/tableish'
+require 'capybara/cucumber'
+require 'capybara/session'
+include Spec::Expectations
+
 
 #Change engine to chrome in order to test a webkit browser
 #Selenium::WebDriver.for :chrome
 
-#Capybara.default_selector = :css
+Capybara.default_selector = :css
 #Capybara.default_driver = :selenium
 
-#Capybara.default_driver = :envjs
-#Capybara.javascript_driver = :envjs
+
+# Kierans
+Capybara.default_driver = :culerity
+Capybara.javascript_driver = :culerity
+Capybara.run_server = false
+
+
 
 # Optional - wenn wir keine UnitTests auf "UnitBasis" verwenden
 # Standardmäßig werden rspec tests verwendet (und auch von uns empfohlen)
 # require 'test/unit/assertions'
 # World(Test::Unit::Assertions)
+
 
 
 # Activate fixtures
@@ -44,33 +55,3 @@ end
 #   fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
 #   Fixtures.create_fixtures(fixtures_folder, fixtures)
 # end
-
-
-
-
-# - - - - - - - Kieran
-# RSpec
-require 'spec/expectations'
-
-# Caybara
-#require 'httparty'
-require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
-#require 'cucumber/web/tableish'
-require 'capybara/cucumber'
-require 'capybara/session'
-include Spec::Expectations
-
-
-#Selenium
-#require "selenium-webdriver"
-
-#Change engine to chrome in order to test a webkit browser
-#Selenium::WebDriver.for :chrome
-
-Capybara.default_selector = :css
-Capybara.default_driver = :culerity
-Capybara.javascript_driver = :culerity
-Capybara.run_server = false
-require 'test/unit/assertions'
-
-World(Test::Unit::Assertions)
