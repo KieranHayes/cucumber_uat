f2f6f25c84dc2c35eefe26bf756fcf942a7e3c24
Christian Trabold <info@christian-trabold.de>
2010-11-19 18:44:39 +0100

    Added new step to check for dates on a page

--- features/steps/new_steps.rb
+++ features/steps/new_steps.rb
@@ -5,3 +5,9 @@ end
 Wenn /^ich das Bild "([^"]*)" anklicke$/ do |arg1|
   find("##{arg1}").click
 end
+
+Then /^I should see a date within "([^"]*)"$/ do |selector|
+  with_scope(selector) do
+      page.text.should match(/(\d{2})\.(\d{2})\.(\d{4})/)
+  end
+end
